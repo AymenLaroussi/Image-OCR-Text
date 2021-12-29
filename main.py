d4d695d3d5995ef3d7bc4a1825db8ac78a0fa2a0
AymenLaroussi <larousi.aymen@gmail.com>
2021-12-29 01:21:36 +0100

    Preparing image before uploading!

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import cv2
+import io
 
 # Import image
 img = cv2.imread("images/1.jpg")
+
+# Get image propriety
+height, width, _ = img.shape
+# Use api ocr from link below
+url_api="https://api.ocr.space/parse/image"
+# Compress image
+_, image_compress = cv2.imencode(".jpg", img, [1, 90])
+file_bytes = io.BytesIO(image_compress)
+
 
 # Display Image
 cv2.imshow("img", img)
