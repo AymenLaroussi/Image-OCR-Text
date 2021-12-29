dfd0fe3c2f4a31709663add60a38ecc35ca31d62
AymenLaroussi <larousi.aymen@gmail.com>
2021-12-29 01:24:47 +0100

    All Done!

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import cv2
 import io
+import requests
+import json
 
 # Import image
 img = cv2.imread("images/1.jpg")
@@ -11,7 +13,17 @@ url_api="https://api.ocr.space/parse/image"
 # Compress image
 _, image_compress = cv2.imencode(".jpg", img, [1, 90])
 file_bytes = io.BytesIO(image_compress)
+# Upload image
+result = requests.post(url_api,
+        files = {"2.jpg": file_bytes},
+        data = {"apikey": "helloworld"})
 
+# Extract Data
+result = result.content.decode()
+result = json.loads(result)
+parsed_results = result.get("ParsedResults")[0]
+text_detected = result.get("ParsedResults")[0].get("ParsedText")
+print(text_detected)
 
 # Display Image
 cv2.imshow("img", img)
